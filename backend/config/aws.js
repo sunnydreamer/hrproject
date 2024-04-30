134cff422221ff9a66f662f9a853d33ceacce296
Maurice Chiu (MBP MacOS Monterey 12.6) <mauricechiu77@gmail.com>
2024-04-29 20:57:51 -0400

    re-touched aws keys

--- backend/config/aws.js
+++ backend/config/aws.js
@@ -1,13 +1,8 @@
 const AWS = require("aws-sdk")
 dotenv = require('dotenv')
-const s3 = new AWS.S3({
-    accessKeyId: "AKIA2PEPIMAG23IHP53H---000",
-    secretAccessKey: "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---000",
-    region: "us-east-1"
-})
 
 
 const bucketName = "testbuckethrproject";
 
 
-module.exports = { s3, bucketName }
+module.exports = { bucketName }
