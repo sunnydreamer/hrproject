4f19d10da427ebd33bc02704e3aff51de1a711fd
sunnydreamer <sunnylee9516@gmail.com>
2024-05-01 16:51:00 -0400

    template

---
+++ frontend/angular-app/src/app/pages/index.ts
@@ -0,0 +1,5 @@
+export * from './employee-profile/employee-profile.component';
+export * from './login/login.component';
+export * from './visa-status/visa-status.component';
+export * from './hiring-management/hiring-management.component';
+export * from './housing-management/housing-management.component';
