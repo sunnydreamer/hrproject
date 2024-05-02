aaf18f9299a56c1770aaf594f12d87d7a81660b7
Maurice Chiu (MBP MacOS Monterey 12.6) <mauricechiu77@gmail.com>
2024-05-02 15:38:48 -0400

    Error page destroys redux state.

--- frontend/react-app/src/pages/ErrorPage.jsx
+++ frontend/react-app/src/pages/ErrorPage.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { RESET_STATE, resetState } from "../redux/redux";
+import store from "../redux/redux";
+// import { fetchData } from "../redux/fetchUserData";
 
 const ErrorPage = (props) => {
   const location = useLocation();
+  const dispatch = useDispatch();
+  // const data = useSelector((state) => state.payload); // Assuming 'data' is the part of the state updated by fetchData
+
+  // useEffect(() => {
+  //   dispatch(fetchData());
+  // }, [dispatch]);
+
+  // useEffect(() => {
+  //   console.log("store.getState()", store.getState()); // You can get data two ways
+  //   console.log("data", data);
+  // }, [data]); // Dependency on 'data' to re-run this effect when 'data' gets loaded
+
+  // Clean up redux store
+  dispatch(resetState());
+
+  // console.log("store.getState()", store.getState()); // You can get data two ways
 
   let errorTitle;
   if (location.state) {
@@ -16,8 +36,11 @@ const ErrorPage = (props) => {
     <div className="full-parent-height">
       <div className="main-content">
         <div className="flex-col centered full-parent-width">
-          <h1 style={{color: "#000"}}>{errorTitle}</h1>
-          <h2>If you are an approved authorized user, please <Link to={"user/login"}>Login</Link></h2>
+          <h1 style={{ color: "#000" }}>{errorTitle}</h1>
+          <h2>
+            If you are an authorized user, please{" "}
+            <Link to={"user/login"}>Login</Link>.
+          </h2>
           <h2>Please contact your HR if you think this is a mistake.</h2>
         </div>
       </div>
